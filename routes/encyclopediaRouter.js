90e77479809b836e4c4882c7419ea21d4ab0252a
Artem <zorkoart123@gmail.com>
2021-12-05 22:16:57 +0100

    first commit

--- routes/encyclopediaRouter.js
+++ routes/encyclopediaRouter.js
@@ -5,9 +5,8 @@ const catalogController = require('../controllers/catalogController')
 router.post("/createPart", catalogController.createPart)
 //router.post("/updatePart", catalogController.updatePart)
 //router.post("/removePart", catalogController.removePart)
-router.get("/:id", catalogController.getCatalog)
 router.get("/getAllParts", catalogController.getAllParts)
 router.get("/getAllOffers", catalogController.getAllOffers)
-router.get("/:id", catalogController.getOne)
+//router.get("/:id", catalogController.getCatalog)
 
 module.exports = router
