e51a70959214f908b479da9ab3d4d0e7b25c70e0
Artem <zorkoart123@gmail.com>
2022-01-07 18:23:51 +0100

    commit 123

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -20,9 +20,7 @@ class CatalogController {
         //list = JSON.parse(JSON.stringify(list))
 
         for (let i = 0; i < list.length; i ++) {
-            const item_id = list[i]["item_id"]
-            const type = list[i]["type"]
-            const time = list[i]["time"]
+            const {item_id, type, time} = list[i]
 
             const view = await View.create({item_id, type, time})
             l.push(view)
